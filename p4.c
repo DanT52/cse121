2193de8c7a6e671c807848cae037efee5c99d64d
DanT52 <dantramp52@gmail.com>
2022-05-03 09:52:47 -0700

    finished p4.c

--- p4.c
+++ p4.c
@@ -1,14 +1,43 @@
 #include<stdio.h>
 
 int main(){
-    
+
     // variables
 
-    int number, count, primenumcount;
-    bool isPrime = TRUE;
+    int nth_prime, test_int, number_of_primes;
+    _Bool isPrime;
+    
 
-    for(test = 2 )
+    test_int = 1;
+    number_of_primes = 0;
+
+    //ask user for nth prime
+    printf("Enter the nth prime to find: ");
+    scanf("%d", &nth_prime);
 
 
+        //check for prime
+
+    while(number_of_primes < nth_prime)
+    {
+
+    test_int++;
+
+    isPrime = 1;
+
+    for(int div = 2; div < test_int; div++){
+        if(test_int % div == 0)
+        {
+            isPrime = 0;
+            break;
+        }
+    }
+        if(isPrime == 1)
+        number_of_primes++;
+    }
+
+    printf("\n The %u Prime Number is %u \n", nth_prime, test_int);
+
+    return 0;
 
 }
