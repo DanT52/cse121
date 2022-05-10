52f828e8eced14933fe8c39067fe7a6a7cc98a5e
DanT52 <dantramp52@gmail.com>
2022-05-10 11:01:11 -0700

    p6.c work on keep chars

--- p6.c
+++ p6.c
@@ -1,9 +1,51 @@
 #include<stdio.h>
 
 
-void make_copy_of_string(char str[], char str_copy[]);
-void keep_chars(char string[]);
-void make_lower(char string[]);
+void make_copy_of_string(char string[], char str_copy[])
+{
+    int i=0;
+    while(string[i]!='\n' && string[i]!='\0')
+    {
+        str_copy[i] = string[i];
+        i++;
+    }
+    str_copy[i] = '\0';
+    string[i]='\0';
+}
+
+void keep_chars(char string[])
+{
+    int i =0;
+    _Bool isLetter = 0;
+
+    while(string[i]!='0')
+    {
+        if (string[i]>='A' && string[i]<='Z' || string[i]>='a' && string[i]<='z')
+        {
+            
+        }
+        else {
+            
+        }
+
+
+
+
+    
+    }
+}
+
+void make_lower(char string[])
+{
+    int i;
+    for(i=0; string[i]!='\0'; i++)
+    {
+    if(string[i]>='A' && string[i]<='Z')
+        {
+            string[i] = string[i] + 32;
+        }
+    }
+}
 _Bool isPalindrome(char string[]);
 
 int main (void) {
@@ -18,12 +60,7 @@ int main (void) {
     make_copy_of_string(phrase, phrase_copy);
     keep_chars(phrase_copy);
     make_lower(phrase_copy);
-
-    if (isPalindrome(phrase_copy) == 1){
-
-
-        
-    }
-
+    printf("lowered: %s", phrase_copy);
 
 }
+
