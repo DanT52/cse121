8305b0ee670fd5fa125988ecf20c384b47adf593
DanT52 <dantramp52@gmail.com>
2022-05-12 10:20:59 -0700

    working version of p6.c

--- p6.c
+++ p6.c
@@ -1,5 +1,67 @@
 #include<stdio.h>
 
+void make_copy_of_string(char string[], char str_copy[]);
+void keep_chars(char string[]);
+void make_lower(char string[]);
+_Bool isPalindrome(char string[]);
+
+int main (void) {
+
+    char phrase[101], phrase_copy[101];
+
+
+    printf("Ender Phrase: ");
+    fgets(phrase, 101, stdin);
+
+
+    make_copy_of_string(phrase, phrase_copy);
+    make_lower(phrase_copy);
+    keep_chars(phrase_copy);
+    
+    printf("modified phrase: %s \n", phrase_copy);
+
+    if (isPalindrome(phrase_copy)){
+        printf("You have a palindrome!!\n");
+    }
+    else{
+        printf("You do not have a palindrome :(\n");
+    }
+
+
+}
+
+_Bool isPalindrome(char string[])
+{
+    int i=0, j=0, h;
+    
+    while(string[i]!='\0'){
+        i++;
+        
+    }
+    
+
+
+    h = i/2;
+
+    
+    
+
+    i--;
+
+    while(j<h){
+        if (string[j]!=string[i]){
+            return 0;
+            
+            
+        }
+
+        i--;
+        j++;
+
+
+    }
+    return 1;
+}
 
 void make_copy_of_string(char string[], char str_copy[])
 {
@@ -15,17 +77,22 @@ void make_copy_of_string(char string[], char str_copy[])
 
 void keep_chars(char string[])
 {
-    int i =0;
-    _Bool isLetter = 0;
+    int i =0, j=0;
 
-    while(string[i]!='0')
+
+    while(string[i]!='\0')
     {
-        if (string[i]>='A' && string[i]<='Z' || string[i]>='a' && string[i]<='z')
+        if (string[i]>='a' && string[i]<='z')
         {
+            string[j]=string[i];
+            i++;
+            j++;
+
             
         }
         else {
-            
+            i++;
+
         }
 
 
@@ -33,6 +100,11 @@ void keep_chars(char string[])
 
     
     }
+
+    string[j] = '\0';
+
+
+    
 }
 
 void make_lower(char string[])
@@ -43,24 +115,10 @@ void make_lower(char string[])
     if(string[i]>='A' && string[i]<='Z')
         {
             string[i] = string[i] + 32;
+            
+            
         }
     }
-}
-_Bool isPalindrome(char string[]);
 
-int main (void) {
-
-    char phrase[101], phrase_copy[101];
-
-
-    printf("Ender Phrase: ");
-    fgets(phrase, 101, stdin);
-
-
-    make_copy_of_string(phrase, phrase_copy);
-    keep_chars(phrase_copy);
-    make_lower(phrase_copy);
-    printf("lowered: %s", phrase_copy);
 
 }
-
